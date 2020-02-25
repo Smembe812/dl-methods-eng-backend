12e430fd418723177298e5cd43b04b8aa5883af5
Smembe812 <psemberekajr@gmail.com>
2020-02-25 18:51:39 +0100

    route test setup passing

--- src/express-routers/knowledge-resource.spec.js
+++ src/express-routers/knowledge-resource.spec.js
@@ -1,9 +1,18 @@
 const request = require('supertest')
-const server = require('../server')('4000')
-
-afterEach( async (done) => await server.close(done));
+const app = require('../')
 
 describe('Knowledge Resource routes', () => {
+    let server;
+    beforeEach( async (done) => {
+        server = await app.listen('4400', () => {
+            console.log('server up at 4400')
+        })
+        
+        done()
+    })
+
+    afterEach( async (done) => await server.close(done));
+
     it('should post new kr', async (done) => {
         request(server)
             .post('/api/knowledge-resources')
