abb03d6fe3ca37bfb11909de68de135d4b276674
Smembe812 <psemberekajr@gmail.com>
2020-02-26 14:26:16 +0100

    get-all-knowledge- resources use case is passing

--- src/use-cases/knowledge-resource/get-knowledge-resources.spec.js
+++ src/use-cases/knowledge-resource/get-knowledge-resources.spec.js
@@ -1,5 +1,13 @@
+const { getAllKnowledgeResources } = require('./index')
+
 describe('get knowledge resources', () => {
-    it.todo('get all knowledge resources')
+    it('should get all knowledge resources', async (done) => {
+        const knowledgeResources = await getAllKnowledgeResources()
+        expect(knowledgeResources.length > 0).toBe((true))
+        done()
+    })
+
+
     it.todo('get knowledge resource by id')
     it.todo('query knowledge resources')
 });
--- src/use-cases/knowledge-resource/index.js
+++ src/use-cases/knowledge-resource/index.js
@@ -2,6 +2,10 @@ const {service} = require('../../data-access/knowledge-resource')
 const makeCreateOneKnowledgeResource = require('./create-one-knowledge-resource')
 const createOneKnowledgeResource = makeCreateOneKnowledgeResource({service})
 
+const makeGetAllKnowledgeResources = require('./get-all-knowledge-resources') 
+const getAllKnowledgeResources = makeGetAllKnowledgeResources({service})
+
 module.exports = {
-    createOneKnowledgeResource
+    createOneKnowledgeResource,
+    getAllKnowledgeResources
 }
