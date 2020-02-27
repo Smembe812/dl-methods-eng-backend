f3fb69f95e73f1ce90b6f05b0d61c52a08c39cc7
Smembe812 <psemberekajr@gmail.com>
2020-02-27 16:53:59 +0100

    delete-one-knowledge-resource controller passing

--- src/controllers/knowledge-resource/index.js
+++ src/controllers/knowledge-resource/index.js
@@ -10,15 +10,19 @@ const makeGetAllKnowledgeResource = require('./get-all-knowledge-resources')
 const {getByIDKnowledgeResources} = require('../../use-cases/knowledge-resource/')
 const makeGetByIDKnowledgeResource = require('./get-by-id-knowledge-resource')
 
+const {deleteOneKnowledgeResource} = require('../../use-cases/knowledge-resource')
+const makeDeleteOneKnowledgeResource = require('./delete-a-knowledge-resource')
 
 const postKnowledgeResource = makePostKnowledgeResource({createOneKnowledgeResource})
 const updateOneKnowledgeResource = makeUpdateKnowledgeResource({updateKnowledgeResource})
 const getAllKnowledgeResourcesController = makeGetAllKnowledgeResource({getAllKnowledgeResources})
 const getByIDKnowledgeResourcesController = makeGetByIDKnowledgeResource({getByIDKnowledgeResources})
+const deleteOneKnowledgeResourceContoller = makeDeleteOneKnowledgeResource({deleteOneKnowledgeResource})
 
 module.exports = {
     postKnowledgeResource,
     updateOneKnowledgeResource,
     getAllKnowledgeResourcesController,
-    getByIDKnowledgeResourcesController
+    getByIDKnowledgeResourcesController,
+    deleteOneKnowledgeResourceContoller
 }
